0d5a9644ad33a343f7991d922ac3f184d5b935ba
tanfeng <tanlover@gmail.com>
2019-02-14 16:12:16 +0800

    Update docker_ssr.sh
    fix

--- docker_ssr.sh
+++ docker_ssr.sh
@@ -2,7 +2,7 @@
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 # centos用 systemctl start docker 启动docker
-docker run -dt --name ssserver --restart=always -p 443:6443 mritd/shadowsocks -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m chacha20-ietf-poly1305 -k “qmhtssr” --fast-open --plugin obfs-server --plugin-opts "obfs=http""
+docker run -dt --name ssserver --restart=always -p 443:6443 mritd/shadowsocks -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m chacha20-ietf-poly1305 -k qmhtssr --fast-open --plugin obfs-server --plugin-opts "obfs=http""
 
 # 检查centos上是否打开了bbr  lsmod | grep tcp_bbr
 
